ae28ddc6e341ed3918f813234d16d24b1fb1f6ce
RamadhanZaki <ramadhanzaki@student.amikom.ac.id>
2025-07-11 14:58:54 +0700

    Update

---
+++ api/projects.js
@@ -0,0 +1,5 @@
+const { projects } = require('../backend/data');
+
+export default function handler(req, res) {
+  res.status(200).json(projects);
+}
---
+++ api/skills.js
@@ -0,0 +1,5 @@
+const { skills } = require('../backend/data');
+
+export default function handler(req, res) {
+  res.status(200).json(skills);
+}
