fa50dcdd010a01bb4d33acdb0e63409240e777b5
RamadhanZaki <amimi100@gmail.com>
2025-07-11 15:38:53 +0700

    Update api v2

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,11 @@
-// backend/index.js
 const express = require('express');
-const cors = require('cors');
+const app = express();
 const { educationHistory, skills, projects } = require('./data');
 
-const app = express();
-const PORT = 3000;
-
-app.use(cors());
 app.use(express.json());
 
 app.get('/api/education', (req, res) => res.json(educationHistory));
 app.get('/api/skills', (req, res) => res.json(skills));
 app.get('/api/projects', (req, res) => res.json(projects));
 
-app.listen(PORT, () => {
-  console.log(`Server backend berjalan di http://localhost:${PORT}`);
-});
+module.exports = app; // penting agar bisa digunakan Vercel
